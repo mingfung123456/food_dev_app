a8e863b90c08e9fc668949ecdf90cca7a991caa7
fung kwing ming <fungming111206@gmail.com>
2020-08-29 20:57:30 +0800

    init app

---
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -0,0 +1,18 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+// 扫描 mybatis 通用 mapper 所在的包
+@MapperScan(basePackages = {"com.imooc.mapper"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
